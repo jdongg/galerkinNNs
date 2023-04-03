7337f3521a9ed340601e7a0b32f881cc742d9f1c
Justin Dong <justindong@Justins-MacBook-Pro.local>
2023-04-03 15:36:50 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # galerkinNNs
-Galerkin neural networks for approximating PDEs
+A neural network framework for approximating PDEs with error control. The framework is described in full detail here:
+
+[1] Ainsworth, M., & Dong, J. (2021). [Galerkin neural networks: A framework for approximating variational equations with error control](https://arxiv.org/abs/2105.14094). SIAM Journal on Scientific Computing, 43(4), A2474-A2501.
+
+This implementation is built on [jax](https://github.com/google/jax) and will also require SciPy and matplotlib. Optional packages include joblib (CPU parallelization of linear coefficient updates) and pickle (for saving trained network parameters for later use).
---
+++ driver_poisson1D_H2.py
@@ -0,0 +1,61 @@
+import numpy as np
+from formulations.galerkinNN_poisson1D_H2 import GNN
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+import os
+
+RESULTS_PATH = 'poisson1D_H2_results'
+if not os.path.exists(RESULTS_PATH):
+    os.makedirs(RESULTS_PATH)
+
+
+MAX_EPOCH = 50		# max epochs per basis function	
+MAX_REFS = 8		# max number of basis functions
+INIT_NEURONS = 5 	# width of network for first basis function
+INIT_SCALE = 1.0 	# scaling parameter for first network: sigma(INIT_SCALE * t)
+eps = 1.e-3 		# penalty for boundary conditions
+
+
+# RHS of Poisson equation
+def source(x):
+	return (2.0*np.pi)**2 * np.sin(2.0*np.pi*x) + 0.1 * (25.0*np.pi)**2 * np.sin(25.0*np.pi*x)
+	# return 2.0*np.ones(x.shape)
+
+
+# true solution
+def exact(x):
+
+	return np.sin(2.0*np.pi*x) + 0.1 * np.sin(25.0*np.pi*x) + 5.0*np.pi*eps*(10.0*eps - 8.0*x + 9.0)/(20.0*eps+10.0)
+	# return x*(1.0-x) + eps
+
+
+# derivative of true solution
+def exactDx(x):
+
+	return (2.0*np.pi) * np.cos(2.0*np.pi*x) + 0.1 * (25.0*np.pi) * np.cos(25.0*np.pi*x) - 40.0*np.pi*eps/(20.0*eps+10.0)
+	# return 1.0 - 2.0*x 
+
+
+# second derivative of true solution
+def exactDx2(x):
+
+	return -(2.0*np.pi)**2 * np.sin(2.0*np.pi*x) - 0.1 * (25.0*np.pi)**2 * np.sin(25.0*np.pi*x)
+
+def main():
+
+	ng = 1024	 		   # number of training points
+	xa = 0.0  	 		   # endpoints of domain
+	xb = 1.0
+	tol = 1.e-9  		   # try to get energy error less than tol
+	gradTol = 0.001        # stop training when relative change in loss < gradTol
+	boundaryPen1 = 1.0/eps # penalty parameters for BCs
+	boundaryPen2 = 1.0/eps 
+
+	model = GNN(INIT_NEURONS, INIT_SCALE, lambda x: source(x), lambda x: exact(x),
+			    lambda x: exactDx(x), lambda x: exactDx2(x), xa, xb, ng, tol, gradTol, boundaryPen1,
+			    boundaryPen2, MAX_EPOCH, MAX_REFS, RESULTS_PATH)
+	model.generateBasis()
+
+if __name__ == '__main__':
+	main()
